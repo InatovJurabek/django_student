6826b0005d59308b7d190d350ee6e93814130fc4
Jurabek <inatovjorabek10@gmail.com>
2025-08-19 20:11:38 +0500

    code

--- main/models.py
+++ main/models.py
@@ -26,6 +26,7 @@ class Student(models.Model):
 class Enrollment(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
     enrolled_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
---
+++ users/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from app.users.models import User
+
+from django import forms 
+from django.contrib import admin 
+from django.contrib 
